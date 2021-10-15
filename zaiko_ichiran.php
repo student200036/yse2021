45d465984499a9147229eb7314820e647b562dd8
kouhei-sugahara <200092tw@yse-c.net>
2021-10-15 14:21:35 +0900

    ⑦～最後まで完成

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -11,36 +11,26 @@
 */
 
 //①セッションを開始する
-
-//②SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-//if (/* ②の処理を書く */){
-	//③SESSIONの「error2」に「ログインしてください」と設定する。
-	//④ログイン画面へ遷移する。
-//}
-
-//⑤データベースへ接続し、接続情報を変数に保存する
-
-//⑥データベースで使用する文字コードを「UTF8」にする
-
-//⑦書籍テーブルから書籍情報を取得するSQLを実行する。また実行結果を変数に保存する
 session_start();
-$_SESSION['login'] = $login;
-$_SESSION['error2'] = $error2;
-if($_SESSION['login'] == false){
-    $_SESSION['error2'] = 'ログインしてください';
-    header('Location: login.php');
+//②SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
+if ($_SESSION['login'] == false){
+	//③SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION['error2'] = 'ログインしてください';
+	//④ログイン画面へ遷移する。
+	header('Location: login.php');
     exit;
 }
 
+//⑤データベースへ接続し、接続情報を変数に保存する
 //$db_connection = 'mysql';
 $db_name = 'zaiko2021_yse';
 $db_host = 'localhost';
 $db_user = 'zaiko2021_yse';
 $db_password = '2021zaiko';
 $db_port = '3306';
-
+//⑥データベースで使用する文字コードを「UTF8」にする
 $dsn = "mysql:dbname={$db_name};host={$db_host};charset=utf8;port={$db_port}";
-
+//$dsn = "dbname={$db_name};host={$db_host};charset=utf8";
 try {
 	$pdo = new PDO($dsn,$db_user,$db_password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -49,11 +39,9 @@ try {
 	echo "接続失敗: ".$e->getMessage();
 	exit;
 }
-
-$dsn = "dbname={$db_name};host={$db_host};charset=utf8";
-
+//⑦書籍テーブルから書籍情報を取得するSQLを実行する。また実行結果を変数に保存する
 $sql = "SELECT * FROM books";
-
+$stmt = $pdo->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -75,9 +63,10 @@ $sql = "SELECT * FROM books";
 				 * ⑧SESSIONの「success」にメッセージが設定されているかを判定する。
 				 * 設定されていた場合はif文の中に入る。
 				 */ 
-				//if(/* ⑧の処理を書く */){
-					//⑨SESSIONの「success」の中身を表示する。
-				//}
+				if(isset($_SESSION['success'])){
+					// ⑨SESSIONの「success」の中身を表示する。
+					echo $_SESSION['success'];
+				}
 				?>
 			</div>
 			
@@ -112,20 +101,21 @@ $sql = "SELECT * FROM books";
 					<tbody>
 						<?php
 						//⑩SQLの実行結果の変数から1レコードのデータを取り出す。レコードがない場合はループを終了する。
-						//while(/* ⑩の処理を書く */){
+						while($book = $stmt->fetch(PDO::FETCH_ASSOC)){
 							//⑪extract変数を使用し、1レコードのデータを渡す。
+							extract($book);
 
-							// echo "<tr id='book'>";
-							// echo "<td id='check'><input type='checkbox' name='books[]'value="./* ⑫IDを設定する */."></td>";
-							// echo "<td id='id'>/* ⑬IDを表示する */</td>";
-							// echo "<td id='title'>/* ⑭titleを表示する */</td>";
-							// echo "<td id='author'>/* ⑮authorを表示する */</td>";
-							// echo "<td id='date'>/* ⑯salesDateを表示する */</td>";
-							// echo "<td id='price'>/* ⑰priceを表示する */</td>";
-							// echo "<td id='stock'>/* ⑱stockを表示する */</td>";
+							echo "<tr id='book'>";
+							echo "<td id='check'><input type='checkbox' name='books[]'value=".$id."></td>";
+							echo "<td id='id'>".$id."</td>";
+							echo "<td id='title'>".$title."</td>";
+							echo "<td id='author'>".$author."</td>";
+							echo "<td id='date'>".$salesDate."</td>";
+							echo "<td id='price'>".$price."</td>";
+							echo "<td id='stock'>".$stock."</td>";
 
-							// echo "</tr>";
-						//}
+							echo "</tr>";
+						}
 						?>
 					</tbody>
 				</table>
